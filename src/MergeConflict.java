679f43707078c676764be2c0c74a1ff3d873ed92
Valentyna Diachuk <valdiachuk9@gmail.com>
2022-10-24 17:17:34 -0500

    new line

--- src/MergeConflict.java
+++ src/MergeConflict.java
@@ -2,6 +2,7 @@ public class MergeConflict {
     public static void main(String[] args) {
         System.out.println("Merge");
         System.out.println("Git is cool");
+        System.out.println("Git is not cool");
 
     }
 }
